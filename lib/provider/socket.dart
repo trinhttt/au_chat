6e9993416d27b79a4a6a1c0c6043c7574f4a9f19
Trinh Thai <ttttrinh132@gmail.com>
2021-10-18 20:41:58 +0700

    update socket

--- lib/provider/socket.dart
+++ lib/provider/socket.dart
@@ -2,11 +2,23 @@ import 'package:au_chat/common/api_config.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketProvider {
-  IO.Socket _socket  = IO.io(APIConfig.baseURL);
+  late IO.Socket _socket;
 
   establishConnection() {
+    _socket = IO.io(
+      APIConfig.baseURL,
+      <String, dynamic>{
+        'transports': ['websocket'],
+        'autoConnect': false,
+      },
+    );
     _socket.connect();
-    print(_socket);
+    _socket.on('connect', (_) => print('Connected'));
+    _socket.on('disconnect', (_) => print('Disconnected'));
+    // _socket.emit("newMessage", {"t1", "t2", "t3"});
+    // _socket.on("messageCreated", (data) {
+    //   print('${data}');
+    // });
   }
 
   closeConnection() {
@@ -15,16 +27,12 @@ class SocketProvider {
 
   addMessage(String messBody, String userId, String channelId) {
     print('messBody $messBody');
-    _socket.onConnect((_) {
-      print('newMessage');
-      _socket.emit("newMessage", { messBody, userId, channelId });
-    });
-
+    _socket.emit("newMessage", {messBody, userId, channelId});
   }
 
   getChatMessage() {
     _socket.on("messageCreated", (data) {
-      print('data[0]data[0] data[0]');
+      // print('data[0]data[0] data[0]');
       String messBody = data[0];
       String userId = data[1];
       String channelId = data[2];
